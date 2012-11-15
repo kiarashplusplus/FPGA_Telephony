e2c920fcbec44775b45eb84dd1223f95e88aa346
nbugg <bugg.ng@gmail.com>
2012-11-15 12:50:23 -0500

    added UI module

---
+++ UI/UI/user_interface.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    12:49:56 11/15/2012 
+// Design Name: 
+// Module Name:    user_interface 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module user_interface(
+    input clk,
+    input s7,
+    input s6,
+    input s5,
+    input s4,
+    input s3,
+    input s2,
+    input s1,
+    input b3,
+    input b2,
+    input b1,
+    input b0,
+    input reset,
+    input enter,
+    input up,
+    input down,
+    input left,
+    input right,
+    output address,
+    output command,
+    output state
+    );
+
+
+endmodule
